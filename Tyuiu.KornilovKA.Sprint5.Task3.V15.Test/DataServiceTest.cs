37cea7acbe9b79cc38b8d784deeb126f7ce1c66b
kilianAid <kilian_bs@mail.ru>
2023-11-17 22:17:44 +0500

    Task3

---
+++ Tyuiu.KornilovKA.Sprint5.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Tyuiu.KornilovKA.Sprint5.Task3.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\KILIAN\source\repos\Tyuiu.KornilovKA.Sprint5\Tyuiu.KornilovKA.Sprint5.Task3.V15\bin\Debug\OutPutFileTask3.bin";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KornilovKA.Sprint5.Task3.V15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KornilovKA.Sprint5.Task3.V15.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint5.Task3.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Корнилов К. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Корнилов Кирилл Андреевич | ИИПб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение f(x) = -1.4x^3 + 2.3x^2 + 0.6x, вычислить его значение   *");
+            Console.WriteLine("* при x = 5, результат сохранить в бинарный файл OutPutFileTask3.bin и    *");
+            Console.WriteLine("* вывести на консоль. Округлить до трёх знаков после запятой.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+
+            Console.WriteLine("X = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            double z = Math.Round(-1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x, 3);
+
+            Console.WriteLine("f(x) = " + z);
+
+            Console.ReadLine();
+        }
+    }
+}
